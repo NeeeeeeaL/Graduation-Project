3b93bf4cb5f513302b13a55f34f5dfc01cdfdc12
NeeeeeeaL <adam0618@outlook.com>
2020-05-12 20:58:38 +0800

    测试了不同图像对应滤波器的参数，换效果不佳

--- FTP_Reconstruction/FTP_Reconstruction/filter.cpp
+++ FTP_Reconstruction/FTP_Reconstruction/filter.cpp
@@ -6,9 +6,9 @@ void filt(const Mat & src, Mat & imgFilt)
 
 {
 	//设计非对称汉宁窗
-	const int hannRows = 22; //原图：50；ROI：22
-	const int hannCols = 410; //原图：1348；ROI：410
-	const int offsetY = 18; //原图：向下偏移44；ROI：18
+	const int hannRows = 30; //原图：50；ROI3：22；ROI4：30；鼠标：32
+	const int hannCols = 800; //原图：1348；ROI3：410；ROI4：750；鼠标：1490
+	const int offsetY = 25; //原图：向下偏移44；ROI3：18；ROI4：25；鼠标：30
 
 	Mat hann = Mat::zeros(hannRows, hannCols, CV_64FC1);
 	for (int i = 0; i < hannRows; ++i)
@@ -16,13 +16,14 @@ void filt(const Mat & src, Mat & imgFilt)
 		for (int j = 0; j < hannCols; ++j)
 		{
 			hann.at<double>(i, j) = 0.5 +
-				0.5 * cos(2.0 * PI * sqrt(pow((i - hannRows / 2.0), 2) / pow(40.0, 2) + pow((j - hannCols / 2.0), 2) / pow(800.0, 2)));
+				0.5 * cos(2.0 * PI * sqrt(pow((i - hannRows / 2.0), 2) / pow(40.0, 2) + pow((j - hannCols / 2.0), 2) / pow(900.0, 2)));
+			//原图：40， 1200；ROI3：35， 700；鼠标：50， 1200
 		}
 	}
 
 	//showImg("Hann", hann);
 	namedWindow("Hann", WINDOW_NORMAL);
-	resizeWindow("Hann", 820, 44);
+	resizeWindow("Hann", 800, 30);
 	imshow("Hann", hann);
 
 	//滤波
@@ -45,7 +46,7 @@ void filt(const Mat & src, Mat & imgFilt)
 			for (int i = centerY + offsetY - (hannRows / 2)-1, n = 0; i < centerY + offsetY + (hannRows / 2)-1, n < hannRows; ++i, ++n)
 			{
 				fftPlanes[p].at<double>(i, j) *= hann.at<double>(n, m);//原为：(i - 840, j - 477)
-				filtPlanes[p].at<double>(i - offsetY - 1, j) = fftPlanes[p].at<double>(i, j);
+				filtPlanes[p].at<double>(i - offsetY, j) = fftPlanes[p].at<double>(i, j);
 			}
 		}
 	}
